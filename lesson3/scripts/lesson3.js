5dc717536aed9200c1ee545f23d01da05c376b2f
vhammon02 <ham20032@byui.edu>
2023-01-29 00:13:06 -0700

    chamber01

---
+++ lesson3/scripts/lesson3.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    document.querySelector("#navbar").classList.toggle('menu-active');
+    document.querySelector("#menu-closed").classList.toggle('menu-active');
+    document.querySelector("#menu-open").classList.toggle('menu-active');
+}
+
